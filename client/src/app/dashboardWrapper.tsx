0a78ef2fd41acf74e4b94649e3a88e36e62753d1
Hannan Abid <hannan@letsusedata.com>
2025-05-09 15:36:04 -0500

    fix project structure

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Navbar from '@/app/(components)/Navbar'
 
 const DashboardWrapper = ({children} : {children: React.ReactNode}) => {
   return <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
@@ -6,7 +7,7 @@ const DashboardWrapper = ({children} : {children: React.ReactNode}) => {
     sidebar
     <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
         {/*navbar*/}
-        navbar
+        <Navbar/>
         {children}
     </main>
   </div>
