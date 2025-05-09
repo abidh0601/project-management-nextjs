5fcdb069586875edc2a2bb4ea0d070f3347bafda
Hannan Abid <hannan@letsusedata.com>
2025-05-09 16:03:09 -0500

    Add sidebar info and pictures for reference

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Navbar from '@/app/(components)/Navbar'
+import Sidebar from './(components)/Sidebar'
 
 const DashboardWrapper = ({children} : {children: React.ReactNode}) => {
   return <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
-    {/* sidebar */}
+    <Sidebar />
     sidebar
     <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
         {/*navbar*/}
