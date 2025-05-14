d76ed46ef2fff8dae19fbdcf189e677b732cd856
Hannan Abid <hannan@letsusedata.com>
2025-05-14 17:25:43 -0500

    Make sidebar responsive and add sidebar buttons

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -22,7 +22,6 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       <Sidebar />
-      sidebar
       <main
         className={`${isSidebarCollapsed ? "" : "md:pl-64"} flex w-full flex-col bg-gray-50 dark:bg-dark-bg`}
       >
