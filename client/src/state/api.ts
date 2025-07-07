5656fe0c58cb4853fb7a24d2a92103c27f402c65
Hannan Abid <hannan.abid@sap.com>
2025-07-07 15:40:36 +0000

    remove margin

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -83,12 +83,12 @@ export const api = createApi({
     baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
     prepareHeaders: async (headers) => {
       const session = await fetchAuthSession();
-      const {accessToken} = session.tokens ?? {};
-      
+      const { accessToken } = session.tokens ?? {};
+
       if (accessToken) {
-        headers.set("Authorization", `Bearer ${accessToken}`)
+        headers.set("Authorization", `Bearer ${accessToken}`);
       }
-      return headers
+      return headers;
     },
   }),
   reducerPath: "api",
